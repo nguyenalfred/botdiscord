de239cc59455f961d01e2cfe7e8d3735954bb6ea
POLYTECHNICHAM <HICHAM.IBOURK@ISEP.FR>
2017-05-22 15:20:54 +0200

    dzd

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
-//toto ti
+// toto ti
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
 })
