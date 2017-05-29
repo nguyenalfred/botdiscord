66fa750198b0bff7a8c8ebf12781b85e7b13ae31
POLYTECHNICHAM <HICHAM.IBOURK@ISEP.FR>
2017-05-29 14:27:30 +0200

    wather api: temperatire of paris

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
-// toto ti
+var httpClient = require('node-rest-client-promise').Client()
+
+//
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
 })
@@ -13,7 +15,16 @@ client.on('message', msg => {
 
   // If message is hello, post hello too
   if (msg.content === 'hello') {
-    msg.channel.sendMessage('Hello to you too, fellow !')
+    msg.channel.sendMessage('Hello to you too!')
+  }
+  // If message is weather, return temperature
+  if (msg.content === 'temperature/paris') {
+    httpClient.getPromise('http://api.openweathermap.org/data/2.5/weather?q=Paris&APPID=b05787eda8d8f7967925692ea52134d2')
+    .then((res) => {
+      var tempK = res.data.main.temp
+      var tempC = tempK - 273.15
+      msg.channel.sendMessage('Température à Paris: ' + tempC.toFixed(2) + ' °C')
+    })
   }
 })
 
