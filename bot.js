c1336f3a4e002fa3eaf53eb8f20b914a9d8f8f28
zhaoyue9207 <yue.zhao@isep.fr>
2017-06-11 21:46:13 +0200

    Premier démarche pour Google_Translate

--- bot.js
+++ bot.js
@@ -37,6 +37,49 @@ client.on('message', msg => {
       msg.channel.sendMessage('Température j+1: ' + forecastTemp[0] + ' , Température j+2: ' + forecastTemp[1] + ' , Température j+3: ' + forecastTemp[2] + ' , Température j+4: ' + forecastTemp[3] + ' , Température j+5: ' + forecastTemp[4])
     })
   }
-})
+  // If message is "translate", post "write your word in english"
+  if (msg.content === 'translate') {
+    msg.channel.sendMessage('Please write the english word')
+	// Imports the Google Cloud client library
+	const Translate = require('@google-cloud/translate');
+		key: 'AIzaSyD5KUAKvMH9TFh-YnFWJ8nVjgkR6yXmklM'
 
-client.login(config.token)
+	// Instantiates a client
+	const translate = Translate();
+
+	// The text to translate, e.g. "Hello, world!"
+	// const text = 'Hello, world!';
+
+	// The target language, e.g. "ru"
+	var target = message.content.charAt(0) + message.content.charAr(1);
+
+	// The model to use, "nmt" or "base"
+	const model = 'base';
+
+	const options = {
+	  // The target language, e.g. "ru"
+	  to: target,
+	  // Make sure your project is whitelisted.
+	  // Possible values are "base" and "nmt"
+	  model: model
+	};
+
+	// Translates the text into the target language. "text" can be a string for
+	// translating a single piece of text, or an array of strings for translating
+	// multiple texts.
+	translate.translate(text, options)
+	  .then((results) => {
+		let translations = results[0];
+		translations = Array.isArray(translations) ? translations : [translations];
+
+		translations.forEach((translation, i) => {
+		  message.channel.send(`${text[i]} => (${target}) ${translation}`);
+		});
+	  })
+	  .catch((err) => {
+		console.error('ERROR:', err);
+	  });
+	  }
+	})
+
+	client.login(config.token)
