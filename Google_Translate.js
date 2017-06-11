11288d0ab64853ee606fd211d3d9789d9df20d5a
zhaoyue9207 <yue.zhao@isep.fr>
2017-06-11 23:52:55 +0200

    correction sur G_T.js

--- Google_Translate.js
+++ Google_Translate.js
@@ -5,7 +5,7 @@
   
   const Command = require('./command')
   // Imports the Google Cloud client library
-  const Translate = require('@google-cloud/translate');
+  const Trans = require('@google-cloud/translate');
 		key: 'AIzaSyD5KUAKvMH9TFh-YnFWJ8nVjgkR6yXmklM'
 		
   module.exports = class Translate extends Command {
@@ -13,43 +13,39 @@
 	static match (message) {
 		return message.content.startsWith('!translate')
 	}
+	  static action (message) {
+    // detection de la langue
+    if (message.content.charAt(2) !== ' ' || message.content.charAt(1) === ' ') {
+      message.channel.send('Veuillez entrer une langue valide.')
+    } else {
+      const translate = Trad
+      var retour = message.content.substring(0)
+      translate.detect(retour)
+      .then((results) => {
+        let detections = results[0]
+        detections = Array.isArray(detections) ? detections : [detections]
+        detections.forEach((detection) => {
+          message.channel.send('Langue de départ : ' + detection.language)
+        })
+      })
+      .catch((err) => {
+        console.log('ERROR', err)
+      })
+      // traduction dans la langue choisie
+      var languedemandee = message.content.charAt(0) + message.content.charAt(1)
+      var atraduire = message.content.substring(3)
+      translate.translate(atraduire, languedemandee).then((results) => {
+        let translations = results[0]
+        translations = Array.isArray(translations) ? translations : [translations]
+        translations.forEach((translation) => {
+          message.channel.send('La traduction : ' + translation)
+        })
+      })
+    .catch((err) => {
+      message.channel.send('ERROR', err)
+    })
+    }
+}
 	
-	static action (message) {
-	// Instantiates a client
-	const translate = Translate();
-
-	// The text to translate, e.g. "Hello, world!"
-	var text = message.content.substring(3);
-
-	// The target language, e.g. "ru"
-	var target = message.content.charAr(1);
-
-	// The model to use, "nmt" or "base"
-	const model = 'base';
-
-	const options = {
-	  // The target language, e.g. "ru"
-	  to: target,
-	  // Make sure your project is whitelisted.
-	  // Possible values are "base" and "nmt"
-	  model: model
-	};
-
-	// Translates the text into the target language. "text" can be a string for
-	// translating a single piece of text, or an array of strings for translating
-	// multiple texts.
-	translate.translate(text, options)
-	  .then((results) => {
-		let translations = results[0];
-		translations = Array.isArray(translations) ? translations : [translations];
-
-		translations.forEach((translation, i) => {
-		  message.channel.send(`${text[i]} => (${target}) ${translation}`);
-		});
-	  })
-	  .catch((err) => {
-		console.error('ERROR:', err);
-	  });
 	  }
-	 }
 	
