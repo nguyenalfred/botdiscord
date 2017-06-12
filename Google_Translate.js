62ba7413aaed76c83eca9bf9e5480942df351c66
POLYTECHNICHAM <HICHAM.IBOURK@ISEP.FR>
2017-06-13 00:12:59 +0200

    debug google translate

--- Google_Translate.js
+++ Google_Translate.js
@@ -1,23 +1,23 @@
-  
-  //------------------------------------------------------//
-  //-------------Google Translate-------------------------//
-  //------------------------------------------------------//
-  
-  const Command = require('./command')
-  //var httpClient = require('node-rest-client-promise').Client()
-  //httpClient.getPromise('https://translation.googleapis.com/language/translate/v2')
-  
-  // Imports the Google Cloud client library
-  const Trans = require('@google-cloud/translate');
-		key: 'AIzaSyD5KUAKvMH9TFh-YnFWJ8nVjgkR6yXmklM'
-		
-  module.exports = class Google_Translate extends Command {
-	  
-	static match (message) {
-		return message.content.startsWith('!translate')
-	}
-	static action (message) {
-		
+
+//------------------------------------------------------//
+//-------------Google Translate-------------------------//
+//------------------------------------------------------//
+
+const Command = require('./command')
+//var httpClient = require('node-rest-client-promise').Client()
+//httpClient.getPromise('https://translation.googleapis.com/language/translate/v2')
+
+// Imports the Google Cloud client library
+const Trans = require('./node_modules/@google-cloud/translate');
+key: 'AIzaSyD5KUAKvMH9TFh-YnFWJ8nVjgkR6yXmklM'
+
+module.exports = class Google_Translate extends Command {
+
+  static match(message) {
+    return message.content.startsWith('!translate')
+  }
+  static action(message) {
+
     if (message.content.charAt(2) !== ' ' || message.content.charAt(1) === ' ') {
       message.channel.send('Veuillez entrer une langue valide.')
     } else {
@@ -32,11 +32,9 @@
           message.channel.send(translation)
         })
       })
-    .catch((err) => {
-      message.channel.send('ERROR', err)
-    })
+        .catch((err) => {
+          message.channel.send('ERROR', err)
+        })
     }
+  }
 }
-	
-	  }
-	
